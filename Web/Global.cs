a7026516c417c753b162ad9d4b07f8c948b4b0a7
IB120084 <IB120084@d21d1406-d953-9443-9845-05699c70ec72>
2018-09-27 22:31:40 +0000

    git-svn-id: http://svn.fit.ba:82/svn/si113@22 d21d1406-d953-9443-9845-05699c70ec72

---
+++ Web/Global.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TransportnoPreduzece.Data.Models;
+
+namespace TransportnoPreduzece.Web
+{
+    public class Global
+    {
+        public static Zaposlenik odabraniVozac { get; set; }
+    }
+}
